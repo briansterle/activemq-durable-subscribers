0eb1826de045caa03dc013c3b0fa01e59c08df9a
Brian Sterle <sterle.brian@gmail.com>
2025-07-31 22:45:51 -0400

    manual ack + thread sleep for better demo logging

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 start_activemq:
 	podman run -d --name activemq -p 61616:61616 -p 8161:8161 rmohr/activemq
 
+restart_activemq:
+	podman restart activemq
 
 publisher: 
 	mvn compile exec:java -Dexec.mainClass=com.sterle.Publisher
@@ -10,7 +12,4 @@ non_durable_subscriber:
 
 durable_subscriber:
 	mvn compile exec:java -Dexec.mainClass=com.sterle.DurableSubscriber
-
-# rm_rf_message_cache:
-# 	podman exec activemq bash -c 'rm -rf /opt/activemq/data/*'
 
--- src/main/java/com/sterle/DurableSubscriber.java
+++ src/main/java/com/sterle/DurableSubscriber.java
@@ -11,18 +11,20 @@ public class DurableSubscriber {
         connection.setClientID("durable-client-id");
         connection.start();
 
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
         Topic topic = session.createTopic("demo.topic");
 
         TopicSubscriber subscriber = session.createDurableSubscriber(topic, "durable-sub");
         System.out.println("Durable subscriber listening for messages...");
         while (true) {
             Message message = subscriber.receive();
+            message.acknowledge();
             if (message instanceof TextMessage textMessage) {
                 System.out.println("Durable received: " + textMessage.getText());
             } else if (message != null) {
                 System.out.println("Received non-text message");
             }
+            Thread.sleep(100);
         }
     }
 }
